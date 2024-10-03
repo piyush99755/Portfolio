c301752c4cf145668c202a83645929802cb4759c
piyush99755 <60968737+piyush99755@users.noreply.github.com>
2024-10-03 17:27:17 +0100

    Added interactivity for project-containers

--- index.js
+++ index.js
@@ -84,6 +84,29 @@ function hideSidebar() {
 showSidebar();
 hideSidebar();
 
+function handleProjectContainer() {
+    let projectContainer = document.querySelectorAll('.project-container');
+    let numOfProjectContainer = projectContainer.length;
+    
+    for(let i = 0; i < numOfProjectContainer; i++){
+        projectContainer[i].addEventListener('mouseover', function() {
+            let containerWidth = parseFloat(getComputedStyle(projectContainer[i]).width);
+            let mouseOverWidth = containerWidth * 1.25;
+            projectContainer[i].style.width= mouseOverWidth + 'px';
+            
+        });
+
+        //Reset the width when the mouse leaves
+        projectContainer[i].addEventListener('mouseout', function() {
+        let originalWidth = parseFloat(getComputedStyle(projectContainer[i]).width) / 1.25;
+        projectContainer[i].style.width = originalWidth + 'px';
+    });
+    }
+    
+}
+
+handleProjectContainer();
+
 
 
 
